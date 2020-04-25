c454b795e1652b5f9d612a5430e007e88d40bd86
Luis De Anda <ldeandac16@gmail.com>
2020-04-25 12:30:36 -0500

    Add home page markup and logic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EventResponse } from '../interfaces';
+import { Subscription } from 'rxjs';
+import { EventsService } from '../events.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  events: EventResponse[] = [];
+  sub: Subscription;
 
-  constructor() {}
+  constructor(private eventSvc: EventsService, private nav: NavController) { }
 
+  ngOnInit(): void {
+    this.sub = this.eventSvc.getAll().subscribe(e => this.events.push(e));
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  getEvents(): EventResponse[] {
+    return this.events.sort((a, b) => a.event.created > b.event.created ? -1 : 1);
+  }
+
+  onClick(id: number) {
+    this.nav.navigateForward(`/details/${id}`);
+  }
 }

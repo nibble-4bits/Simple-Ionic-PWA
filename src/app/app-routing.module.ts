1ceee66f2980f228fe61b188417ca25759f90bf4
Luis De Anda <ldeandac16@gmail.com>
2020-04-25 12:30:46 -0500

    Add details/:eventId route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'details',
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'details/:eventId',
     loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
   },
 ];
